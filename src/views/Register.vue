c1382b9bc001d112f2c9f0a3ff7396cb941b8210
Rafzy <khoirulloh.rafi2@gmail.com>
2021-11-20 18:59:00 +0700

    Slicing Design Register v1

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="login">
+  <section class="register">
     <div class="row">
-      <div class="col-lg-6 col-xl-6 col-xxl-6">
+      <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-6">
         <img src="../../public/images/bg-sign.jpg" alt="bg-sign" class="bg-sign">
         <div class="greeting-text">
           <span class="text">
@@ -10,46 +10,53 @@
         </div>
       </div>
 
-      <div class="col-lg-6 col-xl-6 col-xxl-6 main-content-login">
-        <div class="content-login">
+      <div class="col-md-6 col-lg-6 col-xl-6 col-xxl-6 main-content-register">
+        <div class="content-register">
           <div class="img-app d-flex justify-content-between">
             <img src="../../public/images/app-logo.png" alt="app-logo">
             <h4>Dev App</h4>
           </div>
 
-          <div class="main-content">
-            <h5>Don’t have an account yet? <router-link :to="{ name: 'RegisterApp' }">Create it here</router-link></h5>
-            <hr class="decoration mb-4">
-          </div>
-
           <form action="" method="POST">
-            <div class="form-login">
+            <div class="form-register">
               <div class="form-nis">
                 <label for="nis">Nomor Induk Siswa</label>
                 <input type="number" id="nis" class="form-control" placeholder="Your NIS">
+                <span class="material-icons">tag</span>
+              </div>
+
+              <div class="form-name">
+                <label for="name">Name</label>
+                <input type="text" id="name" class="form-control" placeholder="Your Name">
                 <span class="material-icons">person</span>
+              </div>
+
+              <div class="form-email">
+                <label for="email">Email</label>
+                <input type="text" id="email" class="form-control" placeholder="Your Name">
+                <span class="material-icons">mail</span>
               </div>
 
               <div class="form-password">
                 <label for="password">Password</label>
                 <input type="password" id="password" class="form-control" placeholder="Your Password">
                 <span class="material-icons">https</span>
-                <a href="forgotpassword">Forgot your password?</a>
+                <p>Have account already?</p> <router-link :to="{ name: 'LoginApp' }">Please go to Sign In</router-link> 
               </div>
 
               <div class="btn-submit">
-                <button class="btn btn-primary btn-sm">Sign In</button>
+                <button class="btn btn-primary btn-sm">Sign Up</button>
               </div>
             </div>
           </form>
         </div>
-      </div>      
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'LoginApp'
+  name: 'RegisterApp'
 }
 </script>
