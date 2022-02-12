289df9705e072649b319c3028b54e15edb864505
naufalakmal <muhammadnaufalakmal1@gmail.com>
2022-02-12 10:54:34 +0700

    Add landing page

---
+++ src/components/Footer.vue
@@ -0,0 +1,109 @@
+<template>
+  <!-- Footer Section Start Here -->
+            <footer>
+                <div class="footer-top padding-tb" :style="{ 'background-image' : 'url('+require('@/assets/css/lpcss/bg-image/footer-top-bg-3.jpg')+')'}">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6 col-12">
+                                <div class="footer-item first-set">
+                                    <div class="footer-inner">
+                                        <div class="footer-content">
+                                            <div class="title">
+                                                <h6>About Devlife</h6>
+                                            </div>
+                                            <div class="content pr-3">
+                                                <p>Perusahaan Kami bergerak di bidang jasa software developer yang membuat sebuah website atau produk berbasis software.</p>
+                                                <h6>Alamat</h6>
+                                                <p>Jalan Kliningan Nomor 6 Buah Batu</p>
+                                                <h6>Email</h6>
+                                                <p>devlifecompany@gmail.com</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-12">
+                                <div class="footer-item">
+                                    <div class="footer-inner">
+                                        <div class="footer-content">
+                                            <div class="title">
+                                                <h6>Navigasi</h6>
+                                            </div>
+                                            <div class="content">
+                                                <ul>
+                                                    <li><a href="#">Feature</a></li>
+                                                    <li><a href="#">About</a></li>
+                                                    <li><a href="#">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-12">
+                                <div class="footer-item">
+                                    <div class="footer-inner">
+                                        <div class="footer-content">
+                                            <div class="title">
+                                                <h6>Social Contact</h6>
+                                            </div>
+                                            <div class="content">
+                                                <ul>
+                                                    <li><a href="#"><i class="icofont-facebook"></i>Facebook</a></li>
+                                                    <li><a href="#"><i class="icofont-twitter"></i>Twitter</a></li>
+                                                    <li><a href="https://www.instagram.com/devlife.id/"><i class="icofont-instagram"></i>Instagram</a></li>
+                                                    <li><a href="#"><i class="icofont-youtube"></i>YouTube</a></li>
+                                                    <li><a href="https://github.com/naufalakmal/"><i class="icofont-github"></i>Github</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-12">
+                                <div class="footer-item">
+                                    <div class="footer-inner">
+                                        <div class="footer-content">
+                                            <div class="title">
+                                                <h6>Support Us</h6>
+                                            </div>
+                                            <div class="content">
+                                                <ul>
+                                                    <li><a href="#">Trakteer</a></li>
+                                                    <li><a href="#">Saweria</a></li>
+                                                    <li><a href="#">Dana</a></li>
+                                                    <li><a href="#">Ovo</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-bottom style-2">
+                    <div class="container">
+                        <div class="section-wrapper">
+                            <p>&copy; 2022 All Rights Reserved. Designed by <a href="https://www.instagram.com/naufa_ui/" target="_blank">Muhammad Naufal Akmal</a></p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- Footer Section Ending Here -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1300px !important;
+        text-align: start;
+    }
+}
+</style>
---
+++ src/views/Landingpage.vue
@@ -0,0 +1,210 @@
+<template>
+    <div class="wrapper">
+            <Navbar/>
+            <!-- Banner Section start here -->
+            <section class="banner-section bg_size style-3" :style="{ 'background-image' : 'url('+require('@/assets/images/banner/erp/banner-bg.jpg')+')'}">
+                <div class="round-shape d-none d-lg-block">
+                    <div class="shape round-1"></div>
+                    <div class="shape round-2"></div>
+                    <div class="shape round-3"></div>
+                    <div class="shape round-4"></div>
+                    <div class="shape round-5"></div>
+                </div>
+                <div class="banner-area">
+                    <div class="container">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col-xl-6 col-12">
+                                <div class="content-part">
+                                    <div class="section-header style-2">
+                                        <h2>Face Detector</h2>
+                                        <span>Alat absensi dilengkapi fitur pendeteksi wajah</span>
+                                         <p>Situs web Face Detector adalah situs yang berfungsi untuk mendata kehadiran dengan memindai wajah pengguna dengan lalu data akan tersimpan di database dan akan di anggap hadir apabila telah melakukan pemindaian wajah dan dengan syarat tertentu.</p>
+                                         <router-link :to="{ name: 'Sign Up' }" class="nav-link lab-btn"><span>Create Account</span></router-link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-12">
+                                <div class="section-wrapper">
+                                    <div class="banner-thumb">
+                                        <img src="@/assets/images/thumb.png" alt="lab-banner">
+                                        <div class="thumb-shape">
+                                            <div class="th-shape th-shape-1"><img src="@/assets/images/comp4.png" alt="th-shape"></div>
+                                            <div class="th-shape th-shape-2"><img src="@/assets/images/comp3.png" alt="th-shape"></div>
+                                            <div class="th-shape th-shape-3"><img src="@/assets/images/comp2.png" alt="th-shape"></div>
+                                            <div class="th-shape th-shape-5"><img src="@/assets/images/comp1.png" alt="th-shape"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Banner Section ending here -->
+
+            <!-- About Section Start Here -->
+            <section class="about-section erp-about style-2 padding-tb" id="featured">
+                <div class="container">
+                    <div class="section-wrapper">
+                        <div class="lab-item">
+                            <div class="lab-inner">
+                                <div class="lab-thumb">
+                                    <img src="@/assets/images/about/erp/01.png" alt="lab-thumb">
+                                </div>
+                                <div class="lab-content">
+                                    <h4>Absen Dengan Mudah</h4>
+                                    <p>Absen tidak manual seperti sebelumnya yang di lakukan sebelum KBM yang tentunya akan memakan waktu banyak</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lab-item">
+                            <div class="lab-inner">
+                                <div class="lab-thumb">
+                                    <img src="@/assets/images/about/erp/02.png" alt="lab-thumb">
+                                </div>
+                                <div class="lab-content">
+                                    <h4>Lokasi Absensi harus dalam jangkuan</h4>
+                                    <p>Absen hanya bisa dilakukan saat berada di wilayah sekolah.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lab-item">
+                            <div class="lab-inner">
+                                <div class="lab-thumb">
+                                    <img src="@/assets/images/about/erp/03.png" alt="lab-thumb">
+                                </div>
+                                <div class="lab-content">
+                                    <h4>Dapat Mengajukan Izin</h4>
+                                    <p>User dapat mengajukan izin/sakit untuk tidak hadir ke sekolah, dan harus melampirkan bukti.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- About Section Ending Here -->
+
+            <!-- ERP Manager Section Start Here -->
+            <section class="erp-manager bgc-1 padding-tb">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <div class="lab-thumb">
+                                <div class="abs-lab-thumb">
+                                    <span class="pluse_1"></span>
+                                    <span class="pluse_2"></span>
+                                    <img src="@/assets/images/erp/manager/icon/01.png" alt="lab-erp">
+                                </div>
+                                <img src="@/assets/images/erp/manager/01.png" alt="lab-erp">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 d-flex align-items-center">
+                            <div class="lab-content">
+                                <h2>Tampilan user yang menarik</h2>
+                                <p>website devector memiliki tampilan user interface yang menarik sehingga membuat tampilan lebih nyaman dan enak dipandang.</p>
+                                <a href="#" class="lab-btn"><span>View Details</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="erp-manager padding-tb">
+                <div class="container">
+                    <div class="row flex-row-reverse">
+                        <div class="col-lg-6 col-12">
+                            <div class="lab-thumb">
+                                <div class="abs-lab-thumb pink">
+                                    <span class="pluse_1"></span>
+                                    <span class="pluse_2"></span>
+                                    <img src="@/assets/images/erp/manager/icon/01.png" alt="lab-erp">
+                                </div>
+                                <img src="@/assets/images/erp/manager/02.png" alt="lab-erp">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 d-flex align-items-center">
+                            <div class="lab-content">
+                                <h2>Terdapat banyak fitur yang bisa digunakan</h2>
+                                <p>Website devector memiliki banyak sekali fitur yang dapat digunakan salah satu contohnya terdapat fitur pendeteksi wajah dimana fitur ini akan mendeteksi wajah seseorang dan akan langsung masuk ke dalam database</p>
+                                <a href="#" class="lab-btn"><span>View Details</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="erp-manager bgc-2 padding-tb">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <div class="lab-thumb">
+                                <div class="abs-lab-thumb green">
+                                    <span class="pluse_1"></span>
+                                    <span class="pluse_2"></span>
+                                    <img src="@/assets/images/erp/manager/icon/01.png" alt="lab-erp">
+                                </div>
+                                <img src="@/assets/images/erp/manager/03.png" alt="lab-erp">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-12 d-flex align-items-center">
+                            <div class="lab-content">
+                                <h2>Tampilan halaman admin yang colorfull</h2>
+                                <p>Website devector memiliki tampilan admin yang menarik dan terdapat banyak presentase kehadiran yang membuat perhitungan admin lebih enak dilihat.</p>
+                                <a href="#" class="lab-btn"><span>View Details</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- ERP Manager Section Ending Here -->
+            
+            <!-- Author Level Section Start Here -->
+            <div class="author padding-tb pb-0">
+                <div class="container">
+                    <div class="section-wrapper">
+                        <div class="author-area">
+                            <div class="lab-thumb">
+                                <img src="@/assets/images/erp/manager/author/logo.png" alt="lab-author" width="120">
+                            </div>
+                            <div class="lab-content">
+                                <p class="mb-4">Tentang SMKN 4 BANDUNG</p>
+                                <span>SMKN 4 BANDUNG adalah Sekolah Menengah Kejuruan yang memiliki kelompok bidang keahlian Ketenaga Listrikan, Audio Video, dan Teknik Komputer dan Informatika. Keberadaannya didukung oleh dunia usaha dan dunia industri, baik dalam pembelajaran maupun penyerapan lulusannya. Pembelajaran teori dan praktek tidak hanya dilakukan di dalam kelas tetapi dilakukan di dunia industri melalui praktek kerja industri di perusahaan-perusahaan yang relevan.
+                                <br><br>
+                                Lulusannya telah tersebar di berbagai perguruan tinggi dan Dunia Usaha/Dunia Industri. Kesempatan untuk melanjutkan studi dan bekerja sangat terbuka luas bagi lulusannya. Jalur PMDK tersedia bagi lulusan yang berprestasi baik PN maupun Swasta. Bagi siswa yang ingin bekerja, penempatannya didukung oleh Disnaker melalui Bursa Kerja Khusus (BKK) sesuai dengan kualifikasi yang di persyaratkan oleh perusahaa, serta siswa dapat berwirausaha sesuai dengan kompetensi keahlian masing-masing. </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Author Level Section Start Here -->
+            <Footer/>
+            
+            <!-- scrollToTop start here -->
+            <a href="#" class="scrollToTop"><i class="icofont-swoosh-up"></i><span class="pluse_1"></span><span class="pluse_2"></span></a>
+            <!-- scrollToTop ending here -->
+        </div>
+
+</template>
+
+<script>
+import Navbar from '@/components/LandingNavbar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+    name: 'Landingpage',
+    components: {
+        Navbar,
+        Footer
+    },
+}
+</script>
+
+<style scoped>
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1300px !important;
+        text-align: start;
+    }
+}
+.lab-content span {
+    color: rgba(255, 255, 255, .8);
+}
+</style>
+
